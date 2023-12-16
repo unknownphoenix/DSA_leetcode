c19ac2e88169b036fdf439e05a589dbf0fcece87
unknownphoenix <76801603+unknownphoenix@users.noreply.github.com>
2023-12-16 22:42:44 +0530

    Time: 4 ms (73.04%), Space: 43.6 MB (40.97%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] getFrequency(String str){
+        int[] freq = new int[26];
+        for(int idx=0;idx< str.length();idx++){
+            char ch = str.charAt(idx);
+        }
+        for(char ch : str.toCharArray())
+        freq[ch-'a']++;
+    return freq;
+    }
+    public boolean isAnagram(String s1, String s2) {
+     int[] f1=getFrequency(s1);
+     int[] f2=getFrequency(s2);  
+     for(int idx=0;idx<26;idx++)
+     {
+         if(f1[idx]!=f2[idx])
+           return false;
+     }
+     return true;
+}
+}
